aa2c251042cbe8bf466f1e9796051355739239ad
Steve Tan <i@stevetan.me>
2015-03-25 00:42:51 +0800

    Add task dependency for nodemon

--- gulp/tasks/nodemon.js
+++ gulp/tasks/nodemon.js
@@ -12,7 +12,7 @@ var config = require('../config').nodemon;
 
 var BROWSER_SYNC_RELOAD_DELAY = 500;
 
-gulp.task('nodemon', function (callback) {
+gulp.task('nodemon', ['script:server'], function (callback) {
     var isCalled = false;
 
     return nodemon(config)
