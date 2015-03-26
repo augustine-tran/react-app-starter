48c28ade62027b1044ca4f370533fe4308fe7694
Steve Tan <i@stevetan.me>
2015-03-26 15:26:13 +0800

    Minor fixes

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ if (window != null) {
         var data, hasServerData = false;
         var serverDataDOM = document.getElementById('server-data');
 
-        if (serverDataDOM != null && serverDataDOM.innerText != null && serverDataDOM.innerText.length > 0) {
+        if (serverDataDOM != null && serverDataDOM.innerHTML != null && serverDataDOM.innerHTML.length > 0) {
             try {
                 data = JSON.parse(serverDataDOM.innerHTML);
                 hasServerData = true;
