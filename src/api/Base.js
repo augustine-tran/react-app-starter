11b2f4ae6f17c56c4453a1cfce67ac751bb121d6
Steve Tan <i@stevetan.me>
2015-03-29 21:49:45 +0800

    Updated base API

--- src/api/Base.js
+++ src/api/Base.js
@@ -1,8 +1,10 @@
 'use strict';
 
 // Libraries
+import _ from 'lodash';
 import async from 'async';
 import http from 'superagent';
+import prefix from 'superagent-prefix'
 
 export default class API {
     constructor () {
@@ -11,6 +13,7 @@ export default class API {
 }
 
 API.constants = {
+    BASE_URL: prefix('http://localhost:3000'),
     TIMEOUT_MS: 500
 };
 
--- src/api/User.js
+++ src/api/User.js
@@ -13,7 +13,30 @@ export default class User extends Base {
             function (asyncCallback) {
                 // TODO Refactor this out to a DAO layer.
                 http
-                    .get('http://localhost:3000/test/user/' + id)
+                    .get('/test/user/' + id)
+                    .use(super.constants.BASE_URL)
+                    .timeout(super.constants.TIMEOUT_MS)
+                    .end(asyncCallback);
+            },
+
+            function (result, asyncCallback) {
+                // TODO: Transform the data if necessary.
+                // TODO: Otherwise, pass it back to the caller.
+                asyncCallback(null, result.body);
+            }
+        ], callback);
+    }
+
+    static register(email, name, callback) {
+        async.waterfall([
+            function (asyncCallback) {
+                http
+                    .get('/register')
+                    .query({
+                        email,
+                        name
+                    })
+                    .use(super.constants.BASE_URL)
                     .timeout(super.constants.TIMEOUT_MS)
                     .end(asyncCallback);
             },
