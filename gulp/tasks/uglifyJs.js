bd9897ad2862c57c12c0b3a53e60d612a56cb07b
Steve Tan <i@stevetan.me>
2015-04-16 13:16:48 +0800

    Fixed config in uglifyjs task

--- gulp/tasks/uglifyJs.js
+++ gulp/tasks/uglifyJs.js
@@ -6,7 +6,7 @@ var size = require('gulp-filesize');
 var uglify = require('gulp-uglify');
 
 // Configs
-var config = require('../config').templates;
+var config = require('../config').production;
 
 gulp.task('uglifyJs', ['script'], function () {
     return gulp.src(config.src)
