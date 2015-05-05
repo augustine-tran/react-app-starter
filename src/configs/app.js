1d0e1ad2841607da42b9472557e1776215825f8b
Steve Tan <i@stevetan.me>
2015-05-05 14:12:16 +0800

    Added addthis plugin

--- src/configs/app.js
+++ src/configs/app.js
@@ -4,6 +4,9 @@ export default {
     googleAnalytics: {
         appId: 'UA-xxxxxxxx-x'
     },
+    addthis: {
+        id: 'ra-55485b266c561c10'
+    },
     baseUrl: 'http://react.stevetan.me',
     metadata: {
         title: 'React App Starter',
